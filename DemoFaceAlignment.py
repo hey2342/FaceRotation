51c37f3e73ed5096a95da99f8294bf450647755e
hey2342 <khyooruikzaoin@gmail.com>
2019-04-16 17:21:47 +0900

    only one face detection.

--- DemoFaceAlignment.py
+++ DemoFaceAlignment.py
@@ -5,8 +5,8 @@ from skimage import io
 import numpy as np
 
 
-#changed test
-
+fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
+set_interval = 5
 
 def face_detection(video_dir, dimflag='2d'):
     video_name = os.path.basename(video_dir)[:-3]   #file name
@@ -19,23 +19,46 @@ def face_detection(video_dir, dimflag='2d'):
     print('=====build output file=====')
     if dimflag == '2d':
         video_out = cv2.VideoWriter('./detected/' + video_name + 'mp4', fourcc, fps, (width, height)) #2d_output
+        face_out = cv2.VideoWriter('./detected/' + video_name[:-1] + '_face.mp4', fourcc, fps, (320, 320)) #face_output
     elif dimflag == '3d':
         video_out = cv2.VideoWriter('./detected_3d/' + video_name + 'mp4', fourcc, fps, (width, height)) #3d_output
+        face_out = cv2.VideoWriter('./detected_3d/' + video_name[:-1] + '_face.mp4', fourcc, fps, (320, 320)) #face_output
     else:
         print('#####3d key error#####')
 
     i = -1
     print('=====start face detection======')
+    before_lu = np.array([width, height])
+    before_rd = np.array([width, height])
     while(video.isOpened()):
         ret, frame = video.read()
         i+=1
-        if i % 5 != 0:
+        if i % set_interval != 0:
             continue
         if ret == True:
             preds = face_landmark(frame)
-            w, h = face_box(frame, preds)
-            for j in range(len(w)):
-                save_image(frame[h[j][0]:h[j][1], w[j][0]:w[j][1]], str(i)+'_'+str(j))
+            lu, rd = face_box(frame, preds)
+            save_face = []
+            for j in range(len(lu)):
+                #save_image(frame[lu[j][1]:rd[j][1], lu[j][0]:rd[j][0]], str(i)+'_'+str(j))
+                if i == 0:
+                    if lu[j][0] < before_lu[0]:
+                        save_face = cv2.resize(frame[lu[j][1]:rd[j][1], lu[j][0]:rd[j][0]], (320, 320))
+                        before_lu = lu[j]
+                        before_rd = rd[j]
+                else:
+                    if np.linalg.norm(before_lu - lu[j]) < 50:
+                        save_face = cv2.resize(frame[lu[j][1]:rd[j][1], lu[j][0]:rd[j][0]], (320, 320))
+                        before_lu = lu[j]
+                        before_rd = rd[j]
+                        break
+                    else:
+                        save_face = cv2.resize(frame[before_lu[1]:before_rd[1], before_lu[0]:before_rd[0]], (320, 320))
+            if len(preds) == 0:
+                save_face = cv2.resize(frame[before_lu[1]:before_rd[1], before_lu[0]:before_rd[0]], (320, 320))
+
+            print(i, before_lu, before_rd)
+            face_out.write(save_face)
             image = plot_alignment(frame, preds)
             #if i%500 == 0:
                 #if dimflag == '2d':
@@ -47,10 +70,10 @@ def face_detection(video_dir, dimflag='2d'):
             break
     video.release()
     video_out.release()
+    face_out.release()
     print('=====finish all=====')
         
 def face_landmark(image):
-    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, flip_input=False)
     preds = fa.get_landmarks(image)
     if preds == None:
         preds = []
@@ -71,8 +94,8 @@ def save_image(image, number, dimflag='2d'):
 
 
 def face_box(image, preds):
-    widthls = []
-    heightls = []
+    left_up = []
+    right_down = []
     for i in range(len(preds)):
         width = [int(min(preds[i, : ,0])), int(max(preds[i, : ,0]))]
         height = [int(min(preds[i, : ,1])), int(max(preds[i, :, 1]))]
@@ -95,12 +118,11 @@ def face_box(image, preds):
         if width[1]+right>image.shape[1]:
             width[1] = image.shape[1]
         else:
-            width[1]+=right  
-        widthls.append(width)
-        heightls.append(height)    
+            width[1]+=right
+        left_up.append(np.array([width[0], height[0]]))
+        right_down.append(np.array([width[1], height[1]]))
               
-
-    return widthls, heightls
+    return left_up, right_down
 
 if __name__ == '__main__':
     input_dir = sys.argv[1]
